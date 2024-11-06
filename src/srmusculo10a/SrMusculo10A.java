0307baa22db7a2bb8f91568c2be4312e48fab24d
jacintofranco <jacinto.franco@ifmt.edu.br>
2024-11-06 09:55:40 -0300

    teste

---
+++ src/srmusculo10a/SrMusculo10A.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package srmusculo10a;
+
+/**
+ *
+ * @author geekaia
+ */
+public class SrMusculo10A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JaneladeLogin login = new JaneladeLogin(); 
+        login.setVisible(true);
+        
+    }
+    
+}
